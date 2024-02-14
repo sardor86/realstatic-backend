399553472a1ac48438ff1090e11dc2ede270312b
sardor86 <sshavkatov900@gmail.com>
2024-02-14 17:38:13 +0500

    base prodject

---
+++ RealStatic/settings.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+
+from config import load_config
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+CONFIG = load_config(str(BASE_DIR / '.env'))
+
+
+SECRET_KEY = CONFIG.django.secret_key
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = CONFIG.django.debug
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'RealStatic.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'RealStatic.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': CONFIG.db.db_name,
+        'USER': CONFIG.db.user,
+        'PASSWORD': CONFIG.db.password,
+        'HOST': CONFIG.db.host,
+        'PORT': 5432,
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = 'static/'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ config.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class Django:
+    secret_key: str
+    debug: bool
+
+
+@dataclass
+class DataBase:
+    user: str
+    password: str
+    db_name: str
+    host: str
+
+
+@dataclass
+class Config:
+    django: Django
+    db: DataBase
+
+
+def load_config(path: str) -> Config:
+    env = Env()
+    env.read_env(path)
+
+    return Config(
+                  django=Django(
+                      secret_key=env.str('SECRET_KEY'),
+                      debug=env.bool('DEBUG')
+                  ),
+                  db=DataBase(
+                      db_name=env.str('POSTGRES_DB'),
+                      user=env.str('POSTGRES_USER'),
+                      password=env.str('POSTGRES_PASSWORD'),
+                      host=env.str('DB_HOST')
+                  )
+              )
