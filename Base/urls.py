a6eac43df7da923cdeda65d05548899c44e0c8f9
sardor86 <sshavkatov900@gmail.com>
2024-02-14 19:40:38 +0500

    added about page

--- Base/urls.py
+++ Base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Base.views import home_page
+from Base.views import home_page, about_page
 
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('about/', about_page, name='about'),
 ]
--- Base/views.py
+++ Base/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home_page(request) -> HttpResponse:
     return render(request, 'templates/base/index.html')
+
+
+def about_page(request) -> HttpResponse:
+    return render(request, 'templates/base/about.html')
